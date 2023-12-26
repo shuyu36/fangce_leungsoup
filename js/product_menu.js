eccb74c0e5912a33c2378671234c5b07780f7e3b
shuyu36 <sharon87306@gmail.com>
2023-12-26 15:26:26 +0800

    1226

--- js/product_menu.js
+++ js/product_menu.js
@@ -18,27 +18,82 @@ document.addEventListener("click", function (event) {
   }
 });
 
-// 取得所有菜單連結
-var menuLinks = document.querySelectorAll(".menu-link");
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    freeMode: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+    },
+  });
 
-// 監聽每個菜單連結的點擊事件
-menuLinks.forEach(function (link) {
-  link.addEventListener("click", function (event) {
-    event.preventDefault();
+  // 獲取按鈕元素
+  var dropdownButton = document.getElementById("dropdownMenuButton1");
+  var dropdownMenu = document.querySelector(".dropdown-menu");
 
-    // 取得目標菜單的 ID
-    var targetMenuId = link.getAttribute("data-target");
+  // 獲取所有的 dropdown-item 元素
+  var dropdownItems = document.querySelectorAll(".dropdown-item");
 
-    // 移除之前的 active class
-    document.querySelectorAll(".carousel-item").forEach(function (item) {
-      item.classList.remove("active");
+  // 為每個 dropdown-item 添加點擊事件監聽器
+  dropdownItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      console.log("Button clicked!");
+
+      // 獲取點擊的 dropdown-item 的文字
+      var itemName = item.textContent.trim();
+
+      // 設置按鈕的文字為點擊的 dropdown-item 的文字
+      dropdownButton.textContent = itemName;
     });
+  });
 
-    // 將目標菜單加上 active class
-    document.getElementById(targetMenuId).classList.add("active");
+ 
+
+  // 獲取所有菜單鏈接
+  var menuLinks = document.querySelectorAll(".menu-link");
+  // 為每個菜單鏈接添加點擊事件監聽器
+  menuLinks.forEach(function (link, index) {
+    link.addEventListener("click", function (event) {
+      // 阻止點擊事件冒泡
+      event.stopPropagation();
+      // 獲取 data-target 的值
+      var target = link.getAttribute("data-target");
+
+      // 將所有的 menu-link 移除 active 類別
+      menuLinks.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      // 將當前點擊的 menu-link 添加 active 類別
+      link.classList.add("active");
+
+      // 通過 data-target 的值找到對應的索引
+      var index = Array.from(menuLinks).findIndex(function (item) {
+        return item.getAttribute("data-target") === target;
+      });
+
+      // 如果找到索引，則導航到相應的 swiper 幻燈片
+      if (index !== -1) {
+        swiper.slideTo(index);
+      }
+    });
   });
 });
-
-var myCarousel1 = new bootstrap.Carousel(document.getElementById('carouselExampleInterval'));
-var myCarousel2 = new bootstrap.Carousel(document.getElementById('carouselExampleMobile'));
-
